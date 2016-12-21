ac88ff2028676cb9a1770933b688622adc3afacf
Konrad Scherer <Konrad.Scherer@windriver.com>
2016-12-21 18:44:56 -0500

    Enable r10k config with webhook
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- site/profile/manifests/puppetserver.pp
+++ site/profile/manifests/puppetserver.pp
@@ -11,4 +11,32 @@ class profile::puppetserver
       server_environments         => [],
       server_common_modules_path  => [],
   }
+
+  class {
+    'r10k':
+      version         => 'latest',
+      sources         => {
+        'puppet' => {
+          'remote'  => 'git://ala-git.wrs.com/lpd-ops/puppet-control-repo.git',
+          'basedir' => $::settings::environmentpath,
+          'prefix'  => false,
+        },
+      },
+    manage_modulepath => false
+  }
+
+  # Instead of running via mco, run r10k directly
+  class {'r10k::webhook::config':
+    use_mcollective => false,
+  }
+
+  # The hook needs to run as root when not running using mcollective
+  # It will issue r10k deploy environment <branch_from_gitlab_payload> -p
+  # When git pushes happen.
+  class {'r10k::webhook':
+    use_mcollective => false,
+    user            => 'puppet',
+    group           => 'puppet',
+    require         => Class['r10k::webhook::config'],
+  }
 }
