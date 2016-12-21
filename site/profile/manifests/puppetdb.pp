bf19e68184d127c73b1d98137a8ff6eff94dd3f1
Konrad Scherer <Konrad.Scherer@windriver.com>
2016-12-21 16:23:51 -0500

    Disable firewall management by puppetdb
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- site/profile/manifests/puppetdb.pp
+++ site/profile/manifests/puppetdb.pp
@@ -5,6 +5,7 @@ class profile::puppetdb
   # Configure puppetdb and its underlying database
   class {
     'puppetdb':
+      manage_firewall => false
   }
   # Configure the Puppet master to use puppetdb
   class {
