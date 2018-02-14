2f44b4b38122b542d08ba1020fd199782bf0d0da
Konrad Scherer <Konrad.Scherer@windriver.com>
2018-02-14 10:15:56 -0500

    Remove ntp.conf.dhcp if it is present
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- site/profile/manifests/common/clocksync.pp
+++ site/profile/manifests/common/clocksync.pp
@@ -5,6 +5,14 @@ class profile::common::clocksync {
   if $facts['service_provider'] == 'systemd' {
     include ::profile::common::systemd
   } else {
+    # ntp will use conf from dhcp if it is newer and this will override the puppet defined conf
+    file {
+      '/var/lib/ntp/ntp.conf.dhcp':
+        ensure => absent,
+        before => Class['ntp'],
+        notify => Service['ntp'];
+    }
+
     include ::ntp
     Class['profile::common::package_mirrors'] -> Class['ntp']
   }
